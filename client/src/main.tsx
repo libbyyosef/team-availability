b6c19c3e9b82e878596d0e1546e044d66fa201a1
Libby Yosef <libby10113@gmail.com>
2025-09-21 17:21:30 +0300

    no restrict mode

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,15 +1,13 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { ChakraProvider } from "@chakra-ui/react"; // ← no defaultSystem
 import { Provider as JotaiProvider } from "jotai";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <ChakraProvider>
-      <JotaiProvider>
-        <App />
-      </JotaiProvider>
-    </ChakraProvider>
-  </React.StrictMode>
+  <ChakraProvider>
+    <JotaiProvider>
+      <App />
+    </JotaiProvider>
+  </ChakraProvider>
 );
+
